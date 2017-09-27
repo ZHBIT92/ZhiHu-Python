1dd932bb2dc6a2cb628fa05f0d2769b02a552354
XiaKer <843731066@qq.com>
2017-09-27 15:28:07 +0800

    爬取天气和企业信息
    创建文件夹存放信息，scrapy中实现文本、json、数据库等方式存放信息

--- huangye/test.py
+++ huangye/test.py
@@ -1,6 +1,7 @@
 import requests
 import bs4
 import logging
+import os
 
 def get_html(url):
     try:
@@ -10,6 +11,46 @@ def get_html(url):
         return res.text
     except:
         return " Something Wrong！ "
+
+def get_qiye1(url):
+
+    html = get_html(url)
+    soup = bs4.BeautifulSoup(html, 'lxml')
+
+    sum_list = soup.find_all('div', attrs={'class': 'main'})
+    for test in sum_list:
+        company_list = test.find_all('div', attrs={'class': 'ad_list'})
+        for company in company_list:
+             company1_list = company.find_all('a')
+             # print(company1_list)
+             for company1 in company1_list:
+                 title = company1['title']
+                 link = company1['href']
+                 print("公司名：{}\t{}\n ".format(title, link))
+                 base_dir = os.getcwd()
+                 filename = base_dir + '/企业名录/'+title
+                 if os.path.exists(filename):
+                     print('文件存在,爬取中')
+                 else:
+                     os.mkdir(filename)
+             #   get_qiye2(link)
+
+'''
+def get_qiye2(url):
+
+    html = get_html(url)
+    soup = bs4.BeautifulSoup(html, 'lxml')
+
+    # 找到公司分类，发现她们全部都包含在a标签中
+    company1_list = soup.find_all('ul', attrs={'class': 'qiyecont'})
+    url_list = []
+    for company in company1_list:
+        title = company.a['title']
+        link = company.a['href']
+        base_dir = os.getcwd()
+        filename = base_dir + '/企业名录'+title
+        print("企业名录：{}\t{}\n ".format(title, link))
+'''
 
 def get_content(url):
 
@@ -41,19 +82,29 @@ def get_content(url):
                    logging.exception(e)
 
 def main():
-   with open('company_list.csv', 'w+') as f:
+   base_url= 'http://b2b.huangye88.com/'
+   '''
+   base_dir = os.getcwd()
+   sum = base_dir + '/企业名录'
+   if os.path.exists(sum):
+       print('开始爬取企业名录')
+   else:
+       os.mkdir(sum)
+   '''
+   get_qiye1(base_url)
+
+'''
+with open('company_list.csv', 'w+') as f:
             f.write("")
-   i= 1
-   for page in range(1, 500):
+   for page in range(1, 10):
        if page == 1:
            base_url= 'http://b2b.huangye88.com/guangdong/wenduyibiao31'
            get_content(base_url)
-           i = i+1
        else:
            base_url= 'http://b2b.huangye88.com/guangdong/wenduyibiao31/pn'+str(page)+'/'
            get_content(base_url)
-           i = i+1
-   print(i)
+'''
+
 
 if __name__ == '__main__':
     main()
